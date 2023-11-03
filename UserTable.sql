b5788960270089b47056f67827e88c2994317bcd
Yanxin Peng <111324825+Peter00796@users.noreply.github.com>
2023-11-03 10:06:36 -0700

    Game-relevant data table that might be needed
    Just in case we need the Game relevant data table I have these three tables created as in the detailed design, but we certainly can make changes if this gets too complicated or is not needed.

--- UserTable.sql
+++ UserTable.sql
@@ -23,6 +23,24 @@ CREATE TABLE FriendRequests (
     FOREIGN KEY (friendID) REFERENCES Users(userID)
 );
 
+CREATE TABLE GameObjects (
+    GameObjectID INT AUTO_INCREMENT PRIMARY KEY,
+    GameID INT NOT NULL,
+    FOREIGN KEY (GameID) REFERENCES Games(GameID)
+);
+
+CREATE TABLE TextureFiles (
+    TextureFileID INT AUTO_INCREMENT PRIMARY KEY,
+    GameObjectID INT NOT NULL,
+    TextureFilePath VARCHAR(255) NOT NULL,
+    FOREIGN KEY (GameObjectID) REFERENCES GameObjects(GameObjectID)
+);
+
+CREATE TABLE Games (
+    GameID INT AUTO_INCREMENT PRIMARY KEY,
+    ObjectType ENUM('USC', 'Non-USC') NOT NULL
+);
+
 -- creating user:
 -- INSERT INTO Users (username, password, score1, score2, score3) 
 -- VALUES ('admin', 'csci201', 0, 0, 0);
@@ -40,7 +58,6 @@ CREATE TABLE FriendRequests (
 -- FROM Friends AS f
 -- INNER JOIN Users AS u ON f.friendID = u.userID
 -- WHERE f.userID = (SELECT userID FROM Users WHERE username = 'user');
-
 
 -- checking if a username is a friend of the user: (for requesting a friend) 
 -- SELECT f.friendID, u.username AS friendUsername
